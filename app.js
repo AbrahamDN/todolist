0636587f39e912228c51c91ffe1caab62635ebad
AbrahamDN <40493309+AbrahamDN@users.noreply.github.com>
2019-05-10 00:30:44 +0100

    Updated skin + added new list feature
    Gave the ui a color scheme make-over & added a new feature which lets you create new lists.
    
    Header button> Insert list name > Press enter or click the orange submit button

--- app.js
+++ app.js
@@ -1,111 +1,93 @@
-//----------------------------------------------------------------------------//
-//-----------------------------Main Setup-------------------------------------//
-//----------------------------------------------------------------------------//
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-const ejs = require("ejs");
-const date = require(__dirname+"/date.js");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-//connect to mongo database
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-app.set("view engine", "ejs");//Activate EJS's html file (list.ejs)
+const itemsSchema = {
+  name: String
+};
 
-let webPages = [
-  "index.html"/*0*/
-];
+const Item = mongoose.model("Item", itemsSchema);
 
-app.use(express.static("public"));//Static Folder
-app.use(bodyParser.urlencoded({extended: true}));//Activate bodyParser
-
-//-----------------------------Body/ Main/Scripts-------------------------------------//
-
-
-
-const itemsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  }
-});
-
-const Item = new mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Welcome to your to-do list"
+  name: "Welcome to your todolist!"
 });
+
 const item2 = new Item({
-  name: "Hit the + button to add a new item"
+  name: "Hit the + button to add a new item."
 });
+
 const item3 = new Item({
-  name: "<-- Hit the check box to delete an item"
+  name: "<-- Hit this to delete an item."
 });
 
-const defaultItems = [item1, item2, item3];
+const item4 = new Item({
+  name: "You can create a new list by hitting the Plus (+) button on the header above!"
+});
 
+const defaultItems = [item1, item2, item3, item4];
 
 const listSchema = {
-  name: {
-    type: String,
-    required: true
-  },
+  name: String,
   items: [itemsSchema]
 };
 
 const List = mongoose.model("List", listSchema);
 
 
-//Default List
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
   Item.find({}, function(err, foundItems){
 
-    if (foundItems.length === 0){
+    if (foundItems.length === 0) {
       Item.insertMany(defaultItems, function(err){
-        if (err){
+        if (err) {
           console.log(err);
         } else {
-          console.log("Successfully saved default items to DB");
+          console.log("Default items have been applied a list!");
         }
       });
       res.redirect("/");
     } else {
-        res.render("list", {listTitle: "Today", newListItems: foundItems, todaysDate: date.getDate()});
-      }
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
   });
 
 });
 
-///New/Custom List
-app.get("/:newList", function(req, res){
-  const customListName = _.capitalize(req.params.newList);
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err, foundList){
     if (!err){
       if (!foundList){
-
-        console.log("Created "+customListName+" as new list!");
         //Create a new list
         const list = new List({
           name: customListName,
           items: defaultItems
         });
         list.save();
-        res.redirect("/"+customListName);
-
+        res.redirect("/" + customListName);
       } else {
-        console.log("Redirected to "+customListName+" list");
         //Show an existing list
-        res.render("list", {listTitle: foundList.name, newListItems: foundList.items, todaysDate: ""});
+
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
       }
     }
   });
+
 
 
 });
@@ -126,44 +108,44 @@ app.post("/", function(req, res){
     List.findOne({name: listName}, function(err, foundList){
       foundList.items.push(item);
       foundList.save();
-      res.redirect("/"+listName);
+      res.redirect("/" + listName);
     });
   }
-
 });
 
-app.post ("/delete", function(req, res){
+app.post("/customList", function(req, res){
+  const newCustomList = _.capitalize(req.body.customList);
 
+  if (!newCustomList){
+    res.redirect("/")
+  } else {
+    res.redirect("/" + newCustomList);
+  }
+});
+
+app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
 
-  if (listName === "Today"){
+  if (listName === "Today") {
     Item.findByIdAndRemove(checkedItemId, function(err){
       if (!err) {
-          console.log("*An item was deleted from the to-do list*");
-          res.redirect("/");
-        }
+        console.log("Item deleted from list!");
+        res.redirect("/");
+      }
     });
   } else {
-    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}},
-    function(err, results){
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
       if (!err){
         res.redirect("/" + listName);
-      } else {
-        console.log(err);
       }
     });
   }
 
 
-
 });
 
 
-
-
-//----------------------------------------------------------------------------//
-app.listen(port, function(req, res){
-  console.log("Server successfully running on port: "+port);
-})
-//----------------------------------------------------------------------------//
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
